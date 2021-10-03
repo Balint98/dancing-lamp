75abf381499e139b2fa4bb6a59d50b4d41ba6acb
Balint <balintpod@gmail.com>
2021-10-03 23:27:54 +0200

    csv read without async

--- frame-server/server.js
+++ frame-server/server.js
@@ -3,15 +3,16 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const processLineByLine = require('./frame_server.cjs');
 
-processLineByLine().then((result)=>{
-    const wss = new WebSocketServer({ port: 8080 });
-    wss.on('connection', function connection(ws) {
-        let i = 0;
-        setInterval(() => {
-            if(i === result.length){
-                i = 0;
-            }
-            ws.send(JSON.stringify(result[i++]));
-        }, 15.625);
-    });
+const FPS = 64;
+
+const lines = processLineByLine();
+const wss = new WebSocketServer({ port: 8080 });
+wss.on('connection', function connection(ws) {
+    let i = 0;
+    setInterval(() => {
+        if(i === lines.length){
+            i = 0;
+        }
+        ws.send(JSON.stringify(lines[i++]));
+    }, 1000 / FPS);
 });
