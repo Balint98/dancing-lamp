daad8cc3cf65bd6157db20fd620349b352c69ca8
Balint <balintpod@gmail.com>
2021-10-04 18:37:34 +0200

    create ws inside wsServer listener

--- webserver/web-server.js
+++ webserver/web-server.js
@@ -1,15 +1,10 @@
 import { WebSocketServer, WebSocket } from 'ws';
 
-const ws = new WebSocket('ws://localhost:8080');
 const wsServer = new WebSocketServer({ port: 8081 });
 
 wsServer.on('connection', function connection(wss) {
+    const ws = new WebSocket('ws://localhost:8080');
     ws.on('message', function incoming(message) {
-        // console.log("message");
         wss.send(message.toString());
     });
 }); 
-
-ws.on('open', function open(){
-    console.log("connected to ws://localhost:8080");
-});
