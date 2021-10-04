b156776f150c480169b3339dce503d94b558add6
Balint <balintpod@gmail.com>
2021-10-04 18:19:33 +0200

    listener inside wsServer

--- webserver/web-server.js
+++ webserver/web-server.js
@@ -2,20 +2,14 @@ import { WebSocketServer, WebSocket } from 'ws';
 
 const ws = new WebSocket('ws://localhost:8080');
 const wsServer = new WebSocketServer({ port: 8081 });
-let wsSocket;
 
 wsServer.on('connection', function connection(wss) {
-    wsSocket = wss;
+    ws.on('message', function incoming(message) {
+        // console.log("message");
+        wss.send(message.toString());
+    });
 }); 
 
 ws.on('open', function open(){
     console.log("connected to ws://localhost:8080");
-});
-
-ws.on('message', function incoming(message) {
-    try {
-        wsSocket.send(message.toString());
-    } catch (error) {
-        
-    }
 });
