6b34b21c0fb163ce361aa0fa18a9def9af93658a
Balint <balintpod@gmail.com>
2021-10-05 04:05:12 +0200

    work with NestJs server

---
+++ other/socket-example/src/frame.gateway.ts
@@ -0,0 +1,40 @@
+import {
+    MessageBody,
+    OnGatewayConnection,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer,
+    WsResponse,
+  } from '@nestjs/websockets';
+  import { from, Observable } from 'rxjs';
+  import { map } from 'rxjs/operators';
+  import { Server } from 'ws';
+  import * as WebSocket from 'ws';
+
+  @WebSocketGateway(8081)
+  export class FrameGateway implements OnGatewayConnection {
+    @WebSocketServer()
+    server: Server;
+  
+    handleConnection(client: WebSocket, ...args: any[]) {
+        console.log("new client");
+        // let i = 0;
+        // setInterval(()=>{
+        //     client.send("message "+i++);
+        // }, 1000);
+        let frameSocket = new WebSocket('ws://localhost:8080');
+        frameSocket.onmessage = (message)=>{
+            client.send(message.data);
+        };
+    }
+
+    @SubscribeMessage('events')
+    findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
+      return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
+    }
+  
+    @SubscribeMessage('identity')
+    async identity(@MessageBody() data: number): Promise<number> {
+      return data;
+    }
+  }
